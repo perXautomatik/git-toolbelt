c718b4ff45dfeb3907a6618d8cbaa315836656a4
perXautomatik <christoffer.broback@gmail.com>
2023-03-21 21:54:23 +0100

    the other folder

---
+++ JsonSpecifications/ListContainingArrays.ps1
@@ -0,0 +1,12 @@
+﻿$jsonBase = @{}
+$list = New-Object System.Collections.ArrayList
+
+$list.Add(@{"Name"="John";"Surname"="Smith";"OnSubscription"=$true;})
+$list.Add(@{"Name"="Daniel";"Surname"="Cray";"OnSubscription"=$false;})
+$list.Add(@{"Name"="James";"Surname"="Reed";"OnSubscription"=$true;})
+$list.Add(@{"Name"="Jack";"Surname"="York";"OnSubscription"=$false;})
+
+$customers = @{"Customers"=$list;}
+
+$jsonBase.Add("Data",$customers)
+$jsonBase | ConvertTo-Json -Depth 10 | Out-File ".\customers.json"
