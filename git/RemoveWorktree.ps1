d08fc59e9b2bb57021b19055b6cf2bafbbd675bf
perXautomatik <christoffer.broback@gmail.com>
2023-06-05 00:45:42 +0200

    etc

---
+++ git/RemoveWorktree.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module PsIni
+
+function gitRemoveWorktree ($configPath)
+{
+    $iniContent = Get-IniContent -FilePath $configPath
+    $iniContent.core.Remove("worktree") ;
+    $iniContent | Out-IniFile -FilePath $configPath -Force
+}
