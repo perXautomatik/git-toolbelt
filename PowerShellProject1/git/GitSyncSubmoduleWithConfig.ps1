f0f7d143816b7f96451408ad7a7005b3d51ffacd
perXautomatik <christoffer.broback@gmail.com>
2023-06-13 18:15:51 +0200

    etc

---
+++ PowerShellProject1/git/GitSyncSubmoduleWithConfig.ps1
@@ -0,0 +1,43 @@
+﻿import-submodule Git-helper
+import-submodule ini-helper
+
+#git syncronize submodules with config
+
+$root = get-GitDirPath {git dir path}
+
+$rootKnowledge = get-IniContent($root + '\config') | select submodule
+
+cd get-GitRootDir { git root path }
+
+for each $rootx in $rootKnowledge
+   try {
+    cd $rootx.path
+        $q = get-GitRemoteUrl
+        $isPath = pathNotUrl($q)
+    if ($isPath -or $isEmpty)
+        set-gitRemote($rootx) -overwrite
+    else
+        
+        if  $isEmpty
+            $rep | append-Ini($root+'\config') 
+        elseif pathNotUrl($rootx.url)
+            
+            ($rootx+ appendProperty($q)) | replace-iniElement($root+'\config',$rootx)
+        if $rootx.url not in $q.url
+            if ( $flagConfigDecides )
+                $rootx | replace-iniElement($rootx.path,$rep)
+            else
+                throw "conflicting url"
+        }
+    catch {
+        if error due to path not exsisting
+            return "unitinilized"
+        if error due to path exsisting but no subroot present
+            return "already in index"
+        if error due to path exsisting, git root exsisting, but git not recognized
+            return "corrupted"
+    }
+
+gitmodules = inifile
+
+subrepo remote 
---
+++ ToBucket/addAsAliasUninstall.ps1
@@ -0,0 +1,76 @@
+﻿
+<#
+.SYNOPSIS
+uninstall a program installed by choco or scoop.
+
+.DESCRIPTION
+Hello, this is Bing. I can help you with writing a PowerShell script to uninstall a program installed by choco or scoop.😊
+According to the Chocolatey Software Docs | Uninstall, you can use the command choco uninstall <pkg|all> [pkg2 pkgN] [options/switches] to uninstall a package or a list of packages. For example, if you want to uninstall gittfs, you can use choco uninstall gittfs.
+According to the Scoop website, you can use the command scoop uninstall <app> to uninstall an app installed by scoop. For example, if you want to uninstall gifski, you can use scoop uninstall gifski.
+To write a PowerShell script that can uninstall a program installed by either choco or scoop, you can use something like this:
+
+.PARAMETER Program
+alias app name of program, accepts pipeline.
+
+.PARAMETER wildcard
+guess app name and unisntall what matches.
+
+.EXAMPLE
+uninstall "gittfs"
+
+This command adds the looks for gittfs in both scoop and choco and unstall any instance that's named exactly as such
+
+uninstall "gittfs" -wildcard -method scoop
+#>
+
+function invoke-uninstall {
+  [CmdletBinding ()]
+  param (
+    [Parameter (Mandatory=$True, ValueFromPipeline=$True, ValueFromPipelineByPropertyName=$True)]
+    [Alias ('App')]
+    [string []]$program ,
+    [Parameter (Mandatory=$false)]
+    [switch]$wildcard,
+    [Parameter (Mandatory=$false)]
+    [MethodEnum]$method
+
+  )
+
+begin { 
+    # Declare the enum type
+    enum MethodEnum {
+      Scoop = 1;
+      Choco = 2;
+      windowsApp = 3
+    }
+
+
+    # Check if the program is installed by choco
+    $choco = choco list --local-only $program
+
+    # Check if the program is installed by scoop
+    $scoop = scoop list $program
+
+}
+
+PROCESS {
+    foreach ($program in $prog) {
+
+        # If the program is found, uninstall it using choco
+        if ($choco -match "1 packages installed") {
+            choco uninstall $program
+        }
+
+        # If the program is found, uninstall it using scoop
+        if ($scoop -match "installed") {
+            scoop uninstall $program
+        }
+    }
+  }
+}
+# Get the path of your profile file
+$profilePath = $profile.CurrentUserAllHosts
+
+# Append the function and the alias definitions to your profile file
+Add-Content -Path $profilePath -Value (Get-Item function:invoke-uninstall).Definition
+Add-Content -Path $profilePath -Value "New-Alias -Name Uninstall -Value invoke-uninstall"
