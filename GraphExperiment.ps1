9b3a788d35050cd23ae74aeb4f188674f43490e3
perXautomatik <christoffer.broback@gmail.com>
2023-06-19 18:01:09 +0200

    etc

--- GraphExperiment.ps1
+++ GraphExperiment.ps1
@@ -1,47 +1,84 @@
-﻿$bytes = [System.IO.File]::ReadAllBytes('C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.Windows.Forms.dll')
-[System.Reflection.Assembly]::Load($bytes)
-
-# Define the names of the two files to compare
+﻿# Define the names of the two files to compare
+$importFolder = "B:\PF\Gephi"
 $file1 = "C:\ProgramData\scoop\buckets\anderlli0053_DEV-tools\bucket\EverythingToolbar_dodorz.json"
 $file2 = "C:\ProgramData\scoop\buckets\anderlli0053_DEV-tools\bucket\EverythingToolbar.json"
+# Load the assemblies for System.Windows.Forms and System.Windows.Forms.DataVisualization.Charting
 
-# Run git diff command and capture the output
-$command = "git diff --no-index $file1 $file2"
-$output = Invoke-Expression $command
+function PrefixDupes($a)
+{
 
-# Count the number of lines that are different in each file
-$diff1 = ($output | Select-String "^- ").Count
-$diff2 = ($output | Select-String "^\+ ").Count
+    # Assuming $a and $b are two string arrays
+    # Creating an empty hashtable $ha
+    $ha = @{}
 
-# Load the assembly for System.Windows.Forms
-Add-Type -AssemblyName System.Windows.Forms
 
-# Create a directed graph with System.Windows.Forms.DataVisualization.Charting
-$chart = New-Object System.Windows.Forms.DataVisualization.Charting.Chart
-$chart.Width = 300
-$chart.Height = 300
+    # Defining a recursive function that takes a string $l as a parameter
+    function recursive ($l) {
 
-# Add a series for the nodes and set the chart type to Pie
-$series = $chart.Series.Add("Nodes")
-$series.ChartType = "Pie"
+        # Checking if $ha does not contain the key $l
+        if (!$ha.ContainsKey($l)) {
 
-# Add the files as points and label them with the number of differences
-$series.Points.AddXY("$file1 ($diff1)", $diff1)
-$series.Points.AddXY("$file2 ($diff2)", $diff2)
+            # Assigning the value of $i to the key $l
+            $ha[$l] = $i
 
-# Add a series for the edge and set the chart type to Line
-$series = $chart.Series.Add("Edge")
-$series.ChartType = "Line"
+        } else {
 
-# Add an edge from the file with more differences to the file with less differences
-if ($diff1 -gt $diff2) {
-    $series.Points.AddXY(0.25, 0.5)
-    $series.Points.AddXY(0.75, 0.5)
+            # Storing the value of $ha[$l] in a variable $q
+            $q = $ha[$l]
+
+            # Calling the recursive function with a new key "$l+1"
+            recursive ("$l+1")
+
+            # Assigning the value of $q to the new key "$l+1"
+            $ha["$l+1"] = $q
+
+        }
+    }
+
+
+    # Looping through the elements of $a
+    for ($i = 0; $i -lt $a.length; $i++) {
+
+        # Calling the recursive function with the element of $a at index $i
+        recursive ($a[$i])
+    }
+
+    # Sorting the hashtable by value and displaying the result
+    $ha.GetEnumerator() | Sort-Object Value
 }
-else {
-    $series.Points.AddXY(0.75, 0.5)
-    $series.Points.AddXY(0.25, 0.5)
+
+
+# Read the lines of the text files and store them in arrays
+$lines1 = PrefixDupes (Get-Content $file1) 
+$lines2 = PrefixDupes (Get-Content $file2) 
+
+# Create a CSV file for the node list
+$nodeFile = "$importFolder\nodes.csv"
+$nodeHeader = "Id,Label"
+$nodeHeader | Out-File $nodeFile
+
+foreach ($lines in @($lines1,$lines2))
+{
+    # Add the lines of the first text file as nodes with a prefix of 1
+    foreach ($keyvalues in $lines) {
+        $label = $keyvalues.value
+        $id = [$keyvalues.key
+        "$id,$label" | Out-File $nodeFile -Append
+    }
 }
 
-# Save the chart as an image file
-$chart.SaveImage("graph.png", "PNG")
+# Create a CSV file for the edge list
+$edgeFile = "$importFolder\edges.csv"
+$edgeHeader = "Source,Target,Type"
+$edgeHeader | Out-File $edgeFile
+foreach ($lines in @($lines1,$lines2))
+{
+    # Add the edges between the lines of the first text file as directed edges with a prefix of 1
+    for ($i = 0; $i -lt ($lines1.Count - 1); $i++) {
+        $source = $lines1[$i]
+        $target = $lines1[$i+1]
+        "$source,$target,Directed" | Out-File $edgeFile -Append
+    }
+}
+
+# Import the CSV files into Gephi using the Data Laboratory
