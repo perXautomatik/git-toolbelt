2c0ad4696d36094ec0794944f5ae86a2fe327948
Christoffer Brobäck <christoffer.broback@gmail.com>
2023-06-25 19:40:50 +0200

    etc

---
+++ Snipps/CloneWithReference.ps1
@@ -0,0 +1,20 @@
+    function CloneWithReference( $repo, $objectRepo, $path )
+    {
+      
+      $subfolder = New-Item -Path $path -ItemType Directory -Force -ErrorAction Stop
+
+        # Change the current directory to the subfolder
+        try {
+            cd $subfolder.FullName -ErrorAction Stop
+                        
+            git clone --reference $objectRepo $repo 
+            cd ps1 -PassThru
+            Write-Output "---"
+            $file
+            filterByName $file
+            Write-Output "--reniv--"
+        }
+        catch {
+            Write-Error "Failed to change directory to $subfolder"
+        }
+  }
