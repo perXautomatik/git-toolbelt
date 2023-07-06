298c259fc81232e7decf37ddf58e21836a6e620a
perXautomatik <christoffer.broback@gmail.com>
2023-07-07 01:19:51 +0200

    merge-File-With-History.ps1 gpt

---
+++ git/merge-File-With-History.ps1
@@ -0,0 +1,57 @@
+﻿<#can you write me a powershell script that takes a number of files as input, for each file assume each file belonge to the same git repo; begin block; tag with "before merge", select one of the files (arbitarly, if non specified as parameter) as the target file, process block; for each file; move file to a new folder called merged, rename the file to same name as target file, commit this change with message: original relative path in repo, create a tag with index of the for each, reset the repo hard to the before merge tag. end block; for each tag created with index, do merge this tag to repo, resolve the merge by unioning both of the conflicting files#>
+
+# Get the files to process from the command line or use the current directory
+$files = $args
+if ($files -eq $null) {
+    $files = Get-ChildItem -Path . -Recurse -File
+}
+
+# Get the target file from the command line or use the first file
+$target = $args[0]
+if ($target -eq $null) {
+    $target = $files[0]
+}
+
+# Get the name of the target file without the extension
+$targetName = [System.IO.Path]::GetFileNameWithoutExtension($target)
+
+# Create a new folder called merged if it does not exist
+$mergedFolder = "merged"
+if (-not (Test-Path $mergedFolder)) {
+    New-Item -ItemType Directory -Path $mergedFolder
+}
+
+# Create a tag with "before merge" message
+git tag -a "before merge" -m "Before merge"
+
+# Loop through the files and move them to the merged folder with the target name
+foreach ($file in $files) {
+    # Get the relative path of the file in the repo
+    $relativePath = git ls-files --full-name $file
+
+    # Move the file to the merged folder with the target name and extension
+    $newFile = Join-Path $mergedFolder "$targetName$([System.IO.Path]::GetExtension($file))"
+    Move-Item -Path $file -Destination $newFile
+
+    # Commit the change with the relative path as the message
+    git add $newFile
+    git commit -m $relativePath
+
+    # Create a tag with the index of the file as the message
+    git tag -a $files.IndexOf($file) -m $files.IndexOf($file)
+
+    # Reset the repo hard to the before merge tag
+    git reset --hard "before merge"
+}
+
+# Loop through the tags created with index and merge them to the repo
+$tags = git tag -l | Where-Object {$_ -match "\d+"}
+foreach ($tag in $tags) {
+    # Merge the tag to the repo
+    git merge $tag
+
+    # Resolve the merge by unioning both of the conflicting files
+    git config merge.union.driver true
+    git add .
+    git commit -m "Merged tag $tag"
+}
