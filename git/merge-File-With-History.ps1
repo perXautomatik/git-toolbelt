dc96f533bc1598a7bd9da1c84eb1162fe0284a73
perXautomatik <christoffer.broback@gmail.com>
2023-07-07 01:27:47 +0200

    explained

--- git/merge-File-With-History.ps1
+++ git/merge-File-With-History.ps1
@@ -1,4 +1,106 @@
 ﻿<#can you write me a powershell script that takes a number of files as input, for each file assume each file belonge to the same git repo; begin block; tag with "before merge", select one of the files (arbitarly, if non specified as parameter) as the target file, process block; for each file; move file to a new folder called merged, rename the file to same name as target file, commit this change with message: original relative path in repo, create a tag with index of the for each, reset the repo hard to the before merge tag. end block; for each tag created with index, do merge this tag to repo, resolve the merge by unioning both of the conflicting files#>
+# Define a function to create a git tag with a message
+function New-GitTag {
+    param (
+	[Parameter(Mandatory=$true)]
+	[string]$TagName,
+
+	[Parameter(Mandatory=$true)]
+	[string]$TagMessage
+    )
+
+    # Validate the tag name and message
+    if ($TagName -eq $null -or $TagName -eq "") {
+	Write-Error "Tag name cannot be null or empty"
+	return
+    }
+
+    if ($TagMessage -eq $null -or $TagMessage -eq "") {
+	Write-Error "Tag message cannot be null or empty"
+	return
+    }
+
+    # Create the tag with the message
+    git tag -a $TagName -m $TagMessage
+}
+
+# Define a function to get the relative path of a file in the repo
+function Get-GitRelativePath {
+    param (
+	[Parameter(Mandatory=$true)]
+	[string]$FilePath
+    )
+
+    # Validate the file path
+    if ($FilePath -eq $null -or $FilePath -eq "") {
+	Write-Error "File path cannot be null or empty"
+	return
+    }
+
+    if (-not (Test-Path $FilePath)) {
+	Write-Error "File path does not exist"
+	return
+    }
+
+    # Get the relative path of the file in the repo
+    git ls-files --full-name $FilePath
+}
+
+# Define a function to reset the repo hard to a tag
+function Reset-GitHard {
+    param (
+	[Parameter(Mandatory=$true)]
+	[string]$TagName
+    )
+
+    # Validate the tag name
+    if ($TagName -eq $null -or $TagName -eq "") {
+	Write-Error "Tag name cannot be null or empty"
+	return
+    }
+
+    # Reset the repo hard to the tag
+    git reset --hard $TagName
+}
+
+<#
+.SYNOPSIS
+Merges a tag to the repo and resolves conflicts by unioning files.
+
+.DESCRIPTION
+This function merges a tag to the current branch of the repo and resolves any merge conflicts by unioning both of the conflicting files. It also commits the merge with a message containing the tag name.
+
+.PARAMETER TagName
+The name of the tag to merge. This parameter is mandatory and cannot be null or empty.
+
+.EXAMPLE
+Merge-GitTag -TagName "v1.0"
+
+This example merges the tag "v1.0" to the current branch and resolves any conflicts by unioning files.
+#>
+function Merge-GitTag {
+    [CmdletBinding()]
+    param (
+	[Parameter(Mandatory=$true)]
+	[ValidateNotNullOrEmpty()]
+	[string]$TagName
+    )
+
+    try {
+	# Merge the tag to the repo
+	git merge $TagName
+
+	# Resolve the merge by unioning both of the conflicting files
+	git config merge.union.driver true
+	git add .
+	git commit -m "Merged tag $TagName"
+    }
+    catch {
+	# Write an error message and exit
+	Write-Error "Failed to merge tag $TagName: $_"
+	exit 1
+    }
+}
 
 # Get the files to process from the command line or use the current directory
 $files = $args
@@ -21,37 +123,34 @@ if (-not (Test-Path $mergedFolder)) {
     New-Item -ItemType Directory -Path $mergedFolder
 }
 
-# Create a tag with "before merge" message
-git tag -a "before merge" -m "Before merge"
+# Create a tag with "before merge" message using the function defined above
+New-GitTag -TagName "before merge" -TagMessage "Before merge"
 
-# Loop through the files and move them to the merged folder with the target name
+# Loop through the files and move them to the merged folder with the target name using functions defined above
 foreach ($file in $files) {
-    # Get the relative path of the file in the repo
-    $relativePath = git ls-files --full-name $file
+
+    # Get the relative path of the file in the repo using function defined above
+    $relativePath = Get-GitRelativePath -FilePath $file
 
     # Move the file to the merged folder with the target name and extension
     $newFile = Join-Path $mergedFolder "$targetName$([System.IO.Path]::GetExtension($file))"
-    Move-Item -Path $file -Destination $newFile
 
-    # Commit the change with the relative path as the message
-    git add $newFile
-    git commit -m $relativePath
+     Move-Item -Path $file -Destination $newFile
 
-    # Create a tag with the index of the file as the message
-    git tag -a $files.IndexOf($file) -m $files.IndexOf($file)
+     # Commit the change with the relative path as the message
+     git add $newFile
+     git commit -m $relativePath
 
-    # Reset the repo hard to the before merge tag
-    git reset --hard "before merge"
+     # Create a tag with the index of the file as the message using function defined above
+     New-GitTag -TagName $files.IndexOf($file) -TagMessage  $files.IndexOf($file)
+
+     # Reset the repo hard to the before merge tag using function defined above
+     Reset-GitHard -TagName "before merge"
 }
 
-# Loop through the tags created with index and merge them to the repo
+# Loop through the tags created with index and merge them to the repo using function defined above
 $tags = git tag -l | Where-Object {$_ -match "\d+"}
 foreach ($tag in $tags) {
-    # Merge the tag to the repo
-    git merge $tag
-
-    # Resolve the merge by unioning both of the conflicting files
-    git config merge.union.driver true
-    git add .
-    git commit -m "Merged tag $tag"
+    # Merge the tag to the repo and resolve conflicts by unioning files using function defined above
+    Merge-GitTag -TagName $tag
 }
