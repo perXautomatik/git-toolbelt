caf7b1859cd48e2ba3ae808c319fc635e6f38c5e
perXautomatik <christoffer.broback@gmail.com>
2023-07-07 10:40:41 +0200

    described with gpt

--- filehash2.ps1
+++ filehash2.ps1
@@ -1,87 +1,157 @@
 ﻿<#
-can you write me a powershell script that takes a path to a git repo as input, for each untracked or staged file as addition not modification ; delete n discard this file if exsisting in worktree currently.  for each untracked or staged file as addition not modification ; delete one of the files if more than one file is has same content as another of the staged files or untracked files, prioritise deleting untracked files always, do check and search recursivly to make sure we don't rely on already deleted files
+.SYNOPSIS
+Deletes untracked or staged files as additions, not modifications, in a git repo.
+
+.DESCRIPTION
+This script takes a path to a git repo as input and deletes untracked or staged files as additions, not modifications, in the repo. It also deletes one of the files if more than one file has the same content as another of the staged files or untracked files, prioritizing deleting untracked files always. It also deletes and discards the file if it exists in the worktree currently. It uses git status --porcelain to get the list of files and Get-FileHash to get the hash of the file content.
+
+.PARAMETER Repo
+The path to the git repo.
+
+.EXAMPLE
+.\Delete-Files.ps1 -Repo "C:\Users\Användaren\Documents\Obsidian Vault"
+
+This example deletes untracked or staged files as additions, not modifications, in the "C:\Users\Användaren\Documents\Obsidian Vault" repo.
 #>
 
 # Define a function to get the hash of a file content
-function Get-FileHash() {
-param(
-[Parameter(Mandatory=$true)]
-[ValidateNotNullOrEmpty()]
-$path
-)
+<#
+.SYNOPSIS
+Gets the hash of a file content.
 
-$stream = [System.IO.MemoryStream]::new([System.Text.Encoding]::UTF8.GetBytes("Hello"))
+.DESCRIPTION
+This function gets the hash of a file content using System.IO.MemoryStream and System.Text.Encoding.UTF8.GetBytes. It returns the hash as an integer.
 
-Get-FileHash -InputStream $stream -Algorithm MD5
+.PARAMETER Path
+The path to the file.
 
-        $b = Get-Content $path -Raw -ErrorAction SilentlyContinue
-        $a = $b | Out-String
-        $h = ($a).GetHashCode()
-        return $h
+.EXAMPLE
+Get-FileHash -Path "C:\Users\Användaren\Documents\Obsidian Vault\test.txt"
+
+This example gets the hash of the file content of "C:\Users\Användaren\Documents\Obsidian Vault\test.txt".
+#>
+function Get-FileHash {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Path # The path to the file
+    )
+
+    # Try to get the content of the file as a string
+    try {
+        $Content = Get-Content -Path $Path -Raw -ErrorAction Stop
+    }
+    catch {
+        # Write an error message and return null
+        Write-Error "Failed to get the content of $Path: $_"
+        return $null
+    }
+
+    # Convert the content to a byte array using UTF8 encoding
+    $Bytes = [System.Text.Encoding]::UTF8.GetBytes($Content)
+
+    # Create a memory stream from the byte array
+    $Stream = [System.IO.MemoryStream]::new($Bytes)
+
+    # Get the MD5 hash of the stream using Get-FileHash cmdlet
+    $Hash = Get-FileHash -InputStream $Stream -Algorithm MD5
+
+    # Return the hash as an integer
+    return [int]$Hash.Hash
 }
 
 # Define a function to delete a file and discard the changes
-function Delete-File() {
+<#
+.SYNOPSIS
+Deletes a file and discards the changes.
+
+.DESCRIPTION
+This function deletes a file using Remove-Item cmdlet and discards the changes using git restore command. It uses -WhatIf switch to show what would happen without actually deleting or discarding.
+
+.PARAMETER Path
+The path to the file.
+
+.EXAMPLE
+Delete-File -Path "C:\Users\Användaren\Documents\Obsidian Vault\test.txt"
+
+This example deletes and discards "C:\Users\Användaren\Documents\Obsidian Vault\test.txt".
+#>
+function Delete-File {
+    [CmdletBinding()]
     param(
-    [Parameter(Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    $path
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Path # The path to the file
     )
 
-    Remove-Item $path -Force -ErrorAction Ignore -WhatIf
-    #git restore --staged $path
+    # Try to delete the file using Remove-Item cmdlet with -WhatIf switch
+    try {
+        Remove-Item -Path $Path -Force -ErrorAction Stop -WhatIf
+    }
+    catch {
+        # Write an error message and return
+        Write-Error "Failed to delete $Path: $_"
+        return
+    }
+
+    # Try to discard the changes using git restore command with --staged option and -WhatIf switch
+    try {
+        Invoke-Expression "git restore --staged $Path" -ErrorAction Stop -WhatIf
+    }
+    catch {
+        # Write an error message and return
+        Write-Error "Failed to discard $Path: $_"
+        return
+    }
 }
 
-# Get the path to the git repo as a parameter
-$repo = 'C:\Users\Användaren\Documents\Obsidian Vault'
-
+# Get the path to the git repo as a parameter with validation attribute
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateScript({Test-Path $_})]
+    [string]$Repo # The path to the git repo
+)
 
 # Change the current directory to the repo
-Set-Location $repo
+Set-Location $Repo
 
-# Get the list of untracked or staged files as additions, not modifications
-$regex = "[\s]{2,}"
-$files = git status --porcelain | ForEach-Object { ($_ -split $regex)[1] } | Where-Object { $_ -inotmatch "^[\s\t][DM]\s+(.+)" } 
+# Get the list of untracked or staged files as additions, not modifications using git status --porcelain command
+$Regex = "[\s]{2,}"
+$Files = Invoke-Expression "git status --porcelain" | ForEach-Object { ($_ -split $Regex)[1] } | Where-Object { $_ -inotmatch "^[\s\t][DM]\s+(.+)" } 
+
 # Initialize a hashtable to store the file hashes and paths
-$hashes = @{}
+$Hashes = @{}
 
 # Loop through the files
-foreach ($file in $files) {
-    # Get the hash of the file content
-    if($file)
-    {   
-        $hash = Get-FileHash $file
-        
-        Write-Host $file
-        Write-Host $hash
+foreach ($File in $Files) {
+    # Get the hash of the file content using Get-FileHash function
+    $Hash = Get-FileHash $File
 
-        if($hash)
-        {   
-            $q = $hashes.ContainsKey($hash)
-            
-            # Check if the hash already exists in the hashtable
-            if ($q) {
-                wirte-verbos ($file + "------------" + $hashes[$hash])
-                # If yes, delete one of the files with the same content
-                # Prioritize deleting untracked files
-                if ($file -match "^\?\?") {
-                    Delete-File $file
-                }
-                else {
-                    Delete-File $hashes[$hash]
-                    # Update the hashtable with the new file path
-                    $hashes[$hash] = $file
-                }
+    # Check if the hash is not null
+    if ($Hash) {
+        # Check if the hash already exists in the hashtable
+        if ($Hashes.ContainsKey($Hash)) {
+            # If yes, delete one of the files with the same content
+            # Prioritize deleting untracked files
+            if ($File -match "^\?\?") {
+                Delete-File $File
             }
             else {
-                # If no, add the hash and the file path to the hashtable
-                $hashes[$hash] = $file
+                Delete-File $Hashes[$Hash]
+                # Update the hashtable with the new file path
+                $Hashes[$Hash] = $File
+            }
+        }
+        else {
+            # If no, add the hash and the file path to the hashtable
+            $Hashes[$Hash] = $File
 
-                # Check if the file exists in the worktree currently
-                if (Test-Path $file) {
-                    # If yes, delete and discard the file
-                    Delete-File $file
-                }
+            # Check if the file exists in the worktree currently using Test-Path cmdlet
+            if (Test-Path $File) {
+                # If yes, delete and discard the file using Delete-File function
+                Delete-File $File
             }
         }
     }
