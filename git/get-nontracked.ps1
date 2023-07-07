1f968f3b0a43fa3a2a3d5422be6ec16f7b17657c
perXautomatik <christoffer.broback@gmail.com>
2023-07-07 08:17:35 +0200

    with progressbar

--- git/get-nontracked.ps1
+++ git/get-nontracked.ps1
@@ -25,8 +25,13 @@ function Get-NonTrackedPaths {
   # Initialize an empty array to store the valid repository paths
   $ValidRepoPaths = @()
 
-  # Loop through each path in the input list
+  # Loop through each path in the input list with a progress bar
+  $i = 0
   foreach ($Path in $Paths) {
+
+    # Update the progress bar for the outer loop
+    $i++
+    Write-Progress -Activity "Checking paths" -Status "Processing path $i of $($Paths.Count)" -PercentComplete ($i / $Paths.Count * 100)
 
     # Check if the path is a valid directory
     if (Test-Path -Path $Path -PathType Container) {
@@ -34,24 +39,24 @@ function Get-NonTrackedPaths {
       # Check if the path contains a .git folder
       if (Test-Path -Path "$Path\.git") {
 
-	# Change the current location to the path
-	Push-Location -Path $Path
+        # Change the current location to the path
+        Push-Location -Path $Path
 
-	# Invoke git status command using the Invoke-Git function and capture the output
-	try {
-	  $GitStatus = Invoke-Git -Command "status --porcelain --untracked-files=no"
-	}
-	catch {
-	  # Print the error as a verbose message and continue
-	  Write-Verbose $_.Exception.Message
-	  continue
-	}
+        # Invoke git status command using the Invoke-Git function and capture the output
+        try {
+          $GitStatus = Invoke-Git -Command "status --porcelain --untracked-files=no"
+        }
+        catch {
+          # Print the error as a verbose message and continue
+          Write-Verbose $_.Exception.Message
+          continue
+        }
 
-	# Restore the original location
-	Pop-Location
+        # Restore the original location
+        Pop-Location
 
-	# Add the path to the valid repository paths array
-	$ValidRepoPaths += $Path
+        # Add the path to the valid repository paths array
+        $ValidRepoPaths += $Path
 
       }
 
@@ -59,44 +64,54 @@ function Get-NonTrackedPaths {
 
   }
 
-  # Loop through each valid repository path in the input list
+  # Loop through each valid repository path in the input list with a progress bar
+  $i = 0
   foreach ($Path in $ValidRepoPaths) {
+
+    # Update the progress bar for the outer loop
+    $i++
+    Write-Progress -Activity "Checking paths" -Status "Processing path $i of $($ValidRepoPaths.Count)" -PercentComplete ($i / $ValidRepoPaths.Count * 100)
 
     # Assume the path is not tracked by any other path
     $IsTracked = $false
 
-    # Loop through the other valid repository paths in the input list
+    # Loop through the other valid repository paths in the input list with a progress bar
+    $j = 0
     foreach ($OtherPath in $ValidRepoPaths) {
+
+      # Update the progress bar for the inner loop
+      $j++
+      Write-Progress -Activity "Checking other paths" -Status "Processing other path $j of $($ValidRepoPaths.Count)" -PercentComplete ($j / $ValidRepoPaths.Count * 100) -Id 1
 
       # Skip the current path
       if ($OtherPath -ne $Path) {
 
-	# Change the current location to the other path
-	Push-Location -Path $OtherPath
+        # Change the current location to the other path
+        Push-Location -Path $OtherPath
 
-	# Invoke git status command using the Invoke-Git function and capture the output
-	try {
-	  $GitStatus = Invoke-Git -Command "status --porcelain --untracked-files=no"
-	}
-	catch {
-	  # Print the error as a verbose message and continue
-	  Write-Verbose $_.Exception.Message
-	  continue
-	}
+        # Invoke git status command using the Invoke-Git function and capture the output
+        try {
+          $GitStatus = Invoke-Git -Command "status --porcelain --untracked-files=no"
+        }
+        catch {
+          # Print the error as a verbose message and continue
+          Write-Verbose $_.Exception.Message
+          continue
+        }
 
-	# Restore the original location
-	Pop-Location
+        # Restore the original location
+        Pop-Location
 
-	# Check if the output contains the current path as a normal part of repository or as a submodule
-	if ($GitStatus -match [regex]::Escape($Path)) {
+        # Check if the output contains the current path as a normal part of repository or as a submodule
+        if ($GitStatus -match [regex]::Escape($Path)) {
 
-	  # Set the flag to indicate the current path is tracked by the other path
-	  $IsTracked = $true
+          # Set the flag to indicate the current path is tracked by the other path
+          $IsTracked = $true
 
-	  # Break the inner loop
-	  break
+          # Break the inner loop
+          break
 
-	}
+        }
 
       }
 
@@ -113,6 +128,7 @@ function Get-NonTrackedPaths {
   return $NonTrackedPaths
 
 }
+
 
 # Example usage: pass a list of paths as input and get the non-tracked paths as output
 
