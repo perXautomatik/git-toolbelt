ecb49773ada7e18994079c789d18faac9a5a744c
perXautomatik <christoffer.broback@gmail.com>
2023-07-09 00:11:30 +0200

    Moved into apropriate subfolders

--- JsonSpecifications/JsonList.ps1
+++
@@ -1,6 +0,0 @@
-﻿$jsonBase = @{}
-$list = New-Object System.Collections.ArrayList
-$list.Add("Foo")
-$list.Add("Bar")
-$jsonBase.Add("Data",$list)
-$jsonBase | ConvertTo-Json -Depth 10 | Out-File ".\write-list.json"
