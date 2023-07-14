2e586d594c69c05a357f9debed454c40ea3085cc
perXautomatik <christoffer.broback@gmail.com>
2023-07-14 09:17:06 +0200

    Rewrtitten for easier readability

--- ToBucket/content-AsExt.ps1
+++ ToBucket/content-AsExt.ps1
@@ -1,1 +1,38 @@
-function get-contentAsExt ($path,$ext){ $TempFile = New-TemporaryFile ; Get-Content $path | Set-Content $TempFile ; Rename-Item $TempFile ($tempFile.name -replace '.tmp', $ext) -PassThru }
+# .SYNOPSIS
+# A function to get the content of a file and save it as a different extension
+# .PARAMETER Path
+# The path to the file to get the content from
+# .PARAMETER Ext
+# The extension to save the content as, such as .txt or .csv
+function get-contentAsExt {
+  [CmdletBinding()]
+  param (
+    # Validate that the path is not null or empty and is an existing file
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript( { Test-Path $_ -PathType Leaf }, ErrorMessage = 'Invalid path or file not found.')]
+    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [string]
+    $Path,
+
+    # Validate that the extension is not null or empty, starts with a dot, and has at least one character after it
+    [ValidateNotNullOrEmpty()]
+    [ValidatePattern( '^\.\w+$', ErrorMessage = 'Invalid extension format.')]
+    [Parameter(Mandatory)]
+    [string]
+    $Ext
+  )
+
+  try {
+    # Get the content of the file and save it to a temporary file
+    $TempFile = New-TemporaryFile 
+    Get-Content $Path | Set-Content $TempFile -ErrorAction Stop
+
+    # Rename the temporary file with the new extension and return it
+    Rename-Item $TempFile ($tempFile.name -replace '.tmp', $Ext) -PassThru -ErrorAction Stop
+  }
+  catch {
+    # Display the error message and exit the function
+    Write-Error $_.Exception.Message
+    return
+  }
+}
