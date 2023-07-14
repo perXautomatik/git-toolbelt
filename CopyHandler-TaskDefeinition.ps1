88fb23149c4f0886dd01c40c92193e3d7430ef37
perXautomatik <christoffer.broback@gmail.com>
2023-07-14 09:37:38 +0200

    function breakup

--- CopyHandler-TaskDefeinition.ps1
+++ CopyHandler-TaskDefeinition.ps1
@@ -1,23 +1,147 @@
-﻿# Create a new XML document
-$xml = New-Object System.Xml.XmlDocument
+﻿# Define a function to create an XML element with a given name and value
+function New-XmlElement {
+    <#
+    .SYNOPSIS
+    Creates a new XML element with a given name and value.
+    .PARAMETER Name
+    The name of the XML element.
+    .PARAMETER Value
+    The value of the XML element.
+    .EXAMPLE
+    New-XmlElement -Name "DestinationPath" -Value "E:\"
+    #>
+    # Validate the parameters
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
 
-# Create the root element TaskDefinition
-$root = $xml.CreateElement("TaskDefinition")
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Value
+    )
 
-# Create the DestinationPath element and set its value to E:\
-$dest = $xml.CreateElement("DestinationPath")
-$dest.InnerText = "E:\"
-# Append the DestinationPath element to the root element
-$root.AppendChild($dest)
+    # Create a new XML element with the name and value
+    $element = $xml.CreateElement($Name)
+    $element.InnerText = $Value
 
-# Create the OperationType element and set its value to 1
-$op = $xml.CreateElement("OperationType")
-$op.InnerText = "1"
-# Append the OperationType element to the root element
-$root.AppendChild($op)
+    # Return the XML element
+    $element
+}
 
-# Create the SourcePaths element
-$src = $xml.CreateElement("SourcePaths")
+# Define a function to create an XML document with the given elements
+function New-XmlDocument {
+    <#
+    .SYNOPSIS
+    Creates a new XML document with the given elements.
+    .PARAMETER DestinationPath
+    The destination path for the task definition.
+    .PARAMETER OperationType
+    The operation type for the task definition.
+    .PARAMETER SourcePaths
+    The source paths for the task definition.
+    .PARAMETER Version
+    The version for the task definition.
+    .EXAMPLE
+    New-XmlDocument -DestinationPath "E:\" -OperationType "1" -SourcePaths @("C:\Program Files\Copy Handler\libictranslate64u.dll", "C:\Program Files\Copy Handler\License.txt") -Version "1"
+    #>
+    # Validate the parameters
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$DestinationPath,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$OperationType,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$SourcePaths,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Version
+    )
+
+    # Create a new XML document
+    $xml = New-Object System.Xml.XmlDocument
+
+    # Create the root element TaskDefinition
+    $root = $xml.CreateElement("TaskDefinition")
+
+    # Create the DestinationPath element and append it to the root element
+    $dest = New-XmlElement -Name "DestinationPath" -Value $DestinationPath
+    $root.AppendChild($dest)
+
+    # Create the OperationType element and append it to the root element
+    $op = New-XmlElement -Name "OperationType" -Value $OperationType
+    $root.AppendChild($op)
+
+    # Create the SourcePaths element and append it to the root element
+    $src = $xml.CreateElement("SourcePaths")
+
+    # Loop through each source path and create a Path element with its value and append it to the SourcePaths element
+    foreach ($path in $SourcePaths) {
+        $p = New-XmlElement -Name "Path" -Value $path
+        $src.AppendChild($p)
+    }
+
+    $root.AppendChild($src)
+
+    # Create the Version element and append it to the root element
+    $ver = New-XmlElement -Name "Version" -Value $Version
+    $root.AppendChild($ver)
+
+     # Append the root element to the XML document
+     $xml.AppendChild($root)
+
+     # Set the XML declaration with encoding utf-8
+     $decl = $xml.CreateXmlDeclaration("1.0", "utf-8", $null)
+     $xml.InsertBefore($decl, $root)
+
+     # Return the XML document
+     $xml
+
+}
+
+# Define a function to save an XML document to a file with error checking
+function Save-XmlDocument {
+     <#
+     .SYNOPSIS
+     Saves an XML document to a file with error checking.
+     .PARAMETER XmlDocument
+     The XML document to save.
+     .PARAMETER FilePath
+     The file path to save the XML document to.
+     .EXAMPLE
+     Save-XmlDocument -XmlDocument $xml -FilePath "./output.xml"
+     #>
+     # Validate the parameters
+     [CmdletBinding()]
+     param (
+         [Parameter(Mandatory=$true)]
+         [ValidateNotNullOrEmpty()]
+         [System.Xml.XmlDocument]$XmlDocument,
+
+         [Parameter(Mandatory=$true)]
+         [ValidateNotNullOrEmpty()]
+         [string]$FilePath
+
+     )
+
+     # Try to save the XML document to the file path and catch any errors
+     try {
+         Write-Host "Saving XML document to $FilePath..."
+         $XmlDocument.Save($FilePath)
+         Write-Host "XML document saved successfully."
+     }
+     catch {
+         Write-Error "An error occurred while saving the XML document: $_"
+     }
+}
 
 # Create an array of source paths
 $paths = @(
@@ -28,31 +152,8 @@ $paths = @(
     "C:\Program Files\Copy Handler\msvcp120.dll"
 )
 
-# Loop through each source path and create a Path element with its value
-foreach ($path in $paths) {
-    # Create a Path element
-    $p = $xml.CreateElement("Path")
-    # Set its value to the source path
-    $p.InnerText = $path
-    # Append the Path element to the SourcePaths element
-    $src.AppendChild($p)
-}
-
-# Append the SourcePaths element to the root element
-$root.AppendChild($src)
-
-# Create the Version element and set its value to 1
-$ver = $xml.CreateElement("Version")
-$ver.InnerText = "1"
-# Append the Version element to the root element
-$root.AppendChild($ver)
-
-# Append the root element to the XML document
-$xml.AppendChild($root)
-
-# Set the XML declaration with encoding utf-8
-$decl = $xml.CreateXmlDeclaration("1.0", "utf-8", $null)
-$xml.InsertBefore($decl, $root)
+# Create an XML document with the given elements
+$xml = New-XmlDocument -DestinationPath "E:\" -OperationType "1" -SourcePaths $paths -Version "1"
 
 # Save the XML document to a file named output.xml in the current directory
-$xml.Save("./output.xml")
+Save-XmlDocument -XmlDocument $xml -FilePath "./output.xml"
