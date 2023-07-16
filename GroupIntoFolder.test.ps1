8e5da9ed06e39ede86ca5506d409b2cafd6d7047
perXautomatik <christoffer.broback@gmail.com>
2023-07-16 15:25:28 +0200

    [gpt] with test

--- GroupIntoFolder.test.ps1
+++ GroupIntoFolder.test.ps1
@@ -1,8 +1,10 @@
+
+
 Describe "Create-FolderFromFile" {
     # Test suite for Create-FolderFromFile function
     BeforeAll {
         # Import the function from the script file
-        . .\Create-FolderFromFile.ps1
+        . $PSScriptRoot\GroupIntoFolder.ps1
     }
     AfterEach {
         # Remove any files and folders created by the tests
@@ -13,13 +15,13 @@ Describe "Create-FolderFromFile" {
         It "Creates a folder with the same name as the file, excluding any numbers in parentheses at the end" {
             # Test case for this expectation
             # Create a test file
-            New-Item -Path .\Test (1).txt -ItemType File | Out-Null
+            New-Item -Path ".\Test (1).txt" -ItemType File | Out-Null
             # Invoke the function with the test file as input
-            Create-FolderFromFile -FileName .\Test (1).txt
+            Create-FolderFromFile -FileName ".\Test (1).txt"
             # Assert that a folder named Test is created
             Test-Path -Path .\Test | Should -BeTrue
             # Assert that the test file is moved into the folder
-            Test-Path -Path .\Test\Test (1).txt | Should -BeTrue
+            Test-Path -Path ".\Test\Test (1).txt" | Should -BeTrue
         }
         It "Creates a folder with the same name as the file, if the file name does not have any numbers in parentheses" {
             # Test case for this expectation
