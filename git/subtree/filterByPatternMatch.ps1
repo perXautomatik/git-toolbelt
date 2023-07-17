a560ee9dbb9ac7f4903bfaa64a5e4fbaac866d6b
perXautomatik <christoffer.broback@gmail.com>
2023-07-17 11:03:44 +0200

    temporary folder

--- git/subtree/filterByPatternMatch.ps1
+++ git/subtree/filterByPatternMatch.ps1
@@ -1,32 +1,41 @@
 ﻿function gsx () {
 
-    # Define the parameters
-    param (
-      [string]$RepoPath,
-      [string]$MatchString
-    )
+# Define the parameters
+param (
+  [string]$RepoURL,
+  [string]$MatchString
+)
 
-    # Change the current directory to the repo path
-    Set-Location $RepoPath
+# Create a temporary folder
+$TempFolder = New-TemporaryFile | % { Remove-Item -Path $_.FullName -Force; New-Item -ItemType Directory -Path $_.FullName }
+$TempFolder
+# Clone the repo into the temporary folder
+git clone $RepoURL $TempFolder
 
-    # Get all the files in the repo that match the pattern
-    $Files = Get-ChildItem -Recurse -Filter "*$MatchString*"
+# Change the current directory to the repo path
+Set-Location $TempFolder
 
-    # Loop through each file
-    foreach ($File in $Files) {
-      # Get the relative path of the file
-      $FilePath = $File.FullName.Replace($RepoPath, "")
+# Get all the files in the repo that match the pattern
+$Files = Get-ChildItem -Recurse -Filter "*$MatchString*"
 
-      # Check if filter-repo is available
-      if (Get-Command git-filter-repo -ErrorAction SilentlyContinue) {
-        # Use filter-repo to get only the history relevant to the file
-        git filter-repo --path "$FilePath" --force
-      }
-      else {
-        # Use filter-branch to get only the history relevant to the file
-        git filter-branch --tree-filter "git ls-files -z | grep -zv '$FilePath' | xargs -0 rm" --prune-empty --force
-      }
+# Loop through each file
+foreach ($File in $Files) {
+  # Get the relative path of the file
+  $FilePath = $File.FullName.Replace($RepoPath, "")
+
+  # Check if filter-repo is available
+  if (Get-Command git-filter-repo -ErrorAction SilentlyContinue) {
+    # Use filter-repo to get only the history relevant to the file
+    git filter-repo --path "$FilePath" --force
+  }
+  else {
+    # Use filter-branch to get only the history relevant to the file
+    git filter-branch --tree-filter "git ls-files -z | grep -zv '$FilePath' | xargs -0 rm" --prune-empty --force
+  }
     }
+
+return $TempFolder
 }
 
 gsx -RepoPath 'C:\ProgramData\scoop\buckets\anderlli0053_DEV-tools\bucket' -MatchString 'waifu2x*'
+
