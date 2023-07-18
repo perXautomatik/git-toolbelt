c29bb4c35a6eab5b90e98601d3bf1b1c605e7f9a
perXautomatik <christoffer.broback@gmail.com>
2023-07-18 13:04:33 +0200

    Detect cyclic conflict modification

--- SortByConstraints.ps1
+++ SortByConstraints.ps1
@@ -1,69 +1,64 @@
-﻿<#A directed graph approach for sorting a list with constraints is based on the idea of topological sorting, which is a linear order of the nodes in a directed graph such that for every directed edge u v, node u comes before v in the order. This is useful if you want to sort a list of items that have some dependencies between them, such as tasks that need to be done in a certain order.
+﻿<#Define a function to detect cycles in a directed graph#>
+function Detect-CyclesInDirectedGraph ($nodes, $edges) { 
+# Create a hashtable to store the adjacent nodes for each node 
+$adjacent = @{} 
+foreach ($node in $nodes) { 
+# Initialize the list of adjacent nodes to empty 
+$adjacent[$node] = @() } foreach ($edge in $edges) { 
+# Assume that the edge is a pair of nodes 
+$from, $to = $edge 
+# Add the adjacent node to the list 
+$adjacent[$from] += $to }
 
-To implement this approach in PowerShell, you can follow these steps:
-
-Create a function that takes a list of items and a list of constraints as arguments. The constraints can be pairs of items that indicate that the first item must come before the second one in the sorted list.
-Create a hashtable that maps each item to a list of its dependencies, i.e. the items that must come before it in the sorted list. This can be done by iterating over the constraints and adding them to the hashtable.
-Create a stack that will store the sorted items.
-Create a hashtable that keeps track of which items have been visited during the sorting.
-Create a function that performs a depth-first search (DFS) on the directed graph represented by the hashtable of dependencies. This function takes an item and performs the following steps:
-Mark the item as visited in the second hashtable.
-Iterate over its dependencies and call recursively the DFS function for each dependency that has not been visited yet.
-Add the item to the stack when all its dependencies have been processed.
-Iterate over the list of items and call the DFS function for each item that has not been visited yet.
-Return the stack as the sorted list.
-Here is an example of how you can write the code in PowerShell:
-
-Define a function to sort a list with constraints
-#>
-function Sort-ListWithConstraints ($list, $constraints) {
- # Create a hashtable to store the dependencies for each item 
- $dependencies = @{} 
- foreach ($item in $list) { 
- # Initialize the list of dependencies to empty 
- $dependencies[$item] = @() } foreach ($constraint in $constraints) { 
- # Assume that the constraint is a pair of items 
- $before, $after = $constraint # Add the dependency to the list 
- $dependencies[$after] += $before }
-
-# Create a stack to store the sorted items
+# Create a stack to store the sorted nodes
 $stack = New-Object System.Collections.Stack
 
-# Create a hashtable to keep track of which items have been visited
+# Create a hashtable to keep track of which nodes have been visited
 $visited = @{}
 
+# Create another hashtable to keep track of which nodes are in the recursion stack
+$recStack = @{}
+
 # Define a function to perform DFS on the directed graph
-function DFS ($item) {
-    # Mark the item as visited
-    $visited[$item] = $true
-    # Iterate over its dependencies
-    foreach ($dependency in $dependencies[$item]) {
-        # Call recursively DFS for each dependency that has not been visited yet
-        if (-not $visited[$dependency]) {
-            DFS $dependency
+function DFS ($node) {
+    # Mark the node as visited
+    $visited[$node] = $true
+    # Mark the node as in the recursion stack
+    $recStack[$node] = $true
+    # Iterate over its adjacent nodes
+    foreach ($adjNode in $adjacent[$node]) {
+        # Call recursively DFS for each node that has not been visited yet
+        if (-not $visited[$adjNode]) {
+            if (DFS $adjNode) {
+                return $true
+            }
+        }
+        # If an adjacent node is already in the recursion stack, there is a cycle in the graph
+        elseif ($recStack[$adjNode]) {
+            return $true
         }
     }
-    # Add the item to the stack when all its dependencies have been processed
-    $stack.Push($item)
+    # Unmark the node from the recursion stack when all its adjacent nodes have been processed
+    $recStack[$node] = $false
+    # Add the node to the stack when it is popped from the recursion stack
+    $stack.Push($node)
+    return $false
 }
 
-# Iterate over the list of items and call DFS for each item that has not been visited yet
-foreach ($item in $list) {
-    if (-not $visited[$item]) {
-        DFS $item
+# Iterate over the list of nodes and call DFS for each node that has not been visited yet
+foreach ($node in $nodes) {
+    if (-not $visited[$node]) {
+        if (DFS $node) {
+            return $true
+        }
     }
 }
-
-# Return the stack as the sorted list
-return $stack
+return $false
 
 }
 
 #Test the function with an example
-$list = @(“A”, “B”, “C”, “D”, “E”, “F”)
- $constraints = @((“A”, “B”), (“B”, “C”), (“D”, “E”), (“E”, “F”), (“A”, “F”)) 
- $sorted = Sort-ListWithConstraints $list $constraints 
- Write-Output $sorted
- 
-#Output: D A B E C F
-#I hope this helps you implement a directed graph approach for sorting a list with constraints in PowerShell. If you have any other questions or requests, please let me know. I’m always happy to chat with you. 😊
+$nodes = @(“A”, “B”, “C”, “D”, “E”)
+ $edges = @((“A”, “B”), (“B”, “C”), (“C”, “D”), (“D”, “E”), (“E”, “B”))
+  $hasCycle = Detect-CyclesInDirectedGraph $nodes $edges
+   Write-Output $hasCycle
