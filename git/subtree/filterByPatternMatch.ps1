64c80a4a34268356a5fe6e5ebba3e42cd582d691
perXautomatik <christoffer.broback@gmail.com>
2023-07-19 17:40:34 +0200

    [feature] repair refHeads.ps1

--- git/subtree/filterByPatternMatch.ps1
+++ git/subtree/filterByPatternMatch.ps1
@@ -1,4 +1,47 @@
-﻿#must be a functional repo
+﻿function Check-Value { [CmdletBinding()] param (
+
+    # Validate that the ref name is not null or empty
+    [ValidateNotNullOrEmpty()]
+    [string]$refName
+)
+
+# Define a regex pattern to match the bad ref output
+$regex = 'bad ref (.*) [\(]'
+
+# Invoke the git show-ref command and redirect the error output to the standard output
+$output = (Invoke-Expression -Command 'git show-ref 2>&1')
+
+# Check if the output matches the regex pattern
+if ($output -match $regex) {
+    # Get the matched group value
+    $matchedRef = $Matches[1]
+
+    # Check if the matched ref is the same as the ref name parameter
+    if ($matchedRef -eq $refName) {
+        # Write an error message to indicate that the ref name does match the bad ref output
+        Write-Error "The ref name $refName does match the bad ref output: $output"
+    }
+    else {
+    # Construct a message to update the ref to HEAD
+        $message = "Updating reference $matchedRef to HEAD"
+
+        # Write the message to the output
+        Write-Output $message
+
+        # Invoke the git update-ref command to update the ref to HEAD
+        git update-ref $matchedRef HEAD
+
+        # Recursively call the function with the new last value
+        Check-Value -refName $matchedRef
+    }
+}
+else {
+    # Write an error message to indicate that the git show-ref command did not produce a bad ref output
+    Write-Error "The git show-ref command did not produce a bad ref output: $output"
+}
+}
+
+#must be a functional repo
 function gsx () {
 
 # Define the parameters
@@ -84,5 +127,5 @@ Write-Host $TempFolder
 
 }
 
-gsx -RepoURL 'B:\PF\.git\modules\PowerShell' -MatchString 'git'
+gsx -RepoURL 'C:\Users\CrRoot\AppData\Local\Temp\tmpE2B\PowerShellProjectFolder' -MatchString 'git'
 
