19d2fe0b625cec04a93df3aac54b4f949c80ec87
perXautomatik <christoffer.broback@gmail.com>
2023-07-22 13:30:42 +0200

    Reorganizing

--- Aggregation/split-join/Split-TextByRegex.ps1
+++ Aggregation/split-join/Split-TextByRegex.ps1
@@ -1,14 +1,16 @@
 ﻿function Split-TextByRegex {
-<# .SYNOPSIS
-Splits a text file into multiple sections based on a regular expression.
+<#
+.SYNOPSIS
+Splits the text of a file by a regular expression and returns an array of custom objects.
 
-    .DESCRIPTION
-    This function takes a path to a text file and a regular expression as parameters, and returns an array of objects with the start index, end index, and value of each match. The function uses the Select-String cmdlet to find the matches, and then creates custom objects with the properties of each match.
+.DESCRIPTION
+This function splits the text of a file by a regular expression and returns an array of custom objects. Each object represents a text range that starts with a line that matches the regular expression and ends with the line before the next match or the end of the file. Each object has three properties: match, value and linenumber. The match property is the line that matches the regular expression, the value property is an array of lines that follow the match, and the linenumber property is an array of line numbers that correspond to the value property.
 
 .PARAMETER Path
-The path to the text file to split.
+The path of the file to split.
+
 .PARAMETER Regx
-The regular expression that matches the delimiters of each section.
+The regular expression to use for splitting.
 .OUTPUTS
 A custom object of type 'match.range' that contains the match, value and linenumber properties for each section.
 .EXAMPLE
@@ -22,14 +24,21 @@ match   value
 
     [CmdletBinding()]
     param(
+        # Validate that the path parameter is not null or empty and points to an existing file
         [Parameter(Mandatory = $true)]
-        [ValidateScript({Test-Path $_ -ErrorAction stop})] 
+        [ValidateNotNullOrEmpty()]
+		[ValidateScript({Test-Path $_ -ErrorAction stop})]
+		[ValidateScript({Test-Path -Path $_ -PathType Leaf})]
         [string]$Path,
 
+        # Validate that the regx parameter is not null or empty and is a valid regular expression
         [Parameter(Mandatory = $true)]
-        [string][ValidateNotNullOrEmpty()]$Regx
+		[string]
+		[ValidateScript({[regex]::new($_)})]
+		[ValidateNotNullOrEmpty()]$Regx
         
     )
+
     # Get the content of the text file and append the line number to each line
     try {
     $Input = Get-Content -Path $Path -Raw
@@ -38,13 +47,14 @@ match   value
             Write-Error "Could not read the file: $_"
         return
     }
+    # Append the line number to each line as a custom object
     $LineNrAppended = $Input | Select-String -Pattern '.*' | Select-Object LineNumber, Line
 
     # Get the end of file line number
     $EndOfFile = $Input.Length
 
-    # Find the delimiters that match the regular expression
-    $Delimiters = @($LineNrAppended | Where-Object { $_ -match $Regx })
+    # Find all the lines that match the regular expression and store them as an array of custom objects
+    $Delimeters = @($lineNrAppended | Where-Object { $_ -match $Regx })
 
     # Try to split the content by the regular expression
     try {
@@ -57,23 +67,24 @@ match   value
         return
     }
 
-        # Create an array to store the results
-    $Results = @()
+    # Initialize an empty array for the result
+    $TextRange = @()
 
     if($IncNonmatches)
     {
 	
-    # Loop through the delimiters and create a custom object for each section
-    $TextRange = for ($i = 0; $i -lt $Delimiters.length; $i++) {
-        # Get the upper bound of the section by finding the next delimiter or the end of file
+    # Loop through each delimiter in the array
+    for ($i = 0; $i -lt $Delimeters.length; $i++) {
+
+        # Get the line number of the next delimiter or use the end of file as the upper bound
         $Upper = ( $Delimiters | Select-Object -Index ($i+1) ).LineNumber - 1
         if ($Upper -eq -1) { $Upper = $EndOfFile }
 
-        # Get the lines that belong to the section
+        # Get all the lines between the current delimiter and the upper bound as an array of custom objects
         $Section = ($LineNrAppended | Where-Object { $_.LineNumber -in (( $Delimiters | Select-Object -Index $i ).LineNumber .. $Upper) })
 
-        # Create a custom object with the match, value and linenumber properties
-        [PSCustomObject]@{
+        # Create a custom object with match, value and linenumber properties and add it to the result array
+        $TextRange += , [PSCustomObject]@{
             PSTypeName = 'match.range' # Give the object a type name
             match = $Section.Line[0] # The first line is the match
             value = @($Section.Line | Select-Object -Skip 1) # The rest are the values
@@ -93,11 +104,15 @@ match   value
         }
     }
 
-    # Define a default display property set for the custom object type if it doesn't exist already
-    if (-not (Get-TypeData -TypeName 'match.range').DefaultDisplayPropertySet) {
-        Update-TypeData -TypeName 'match.range' -DefaultDisplayPropertySet 'match', 'value'
+    # Check if there is already a type data for match.range objects and update it if not
+    if (!(Get-TypeData -TypeName 'match.range').defaultDisplayPropertySet) {
+        $TypeData = @{
+            TypeName = 'match.range' #refere to object by it's type name
+            DefaultDisplayPropertySet = 'match','value'
+        }
+        Update-TypeData @TypeData
     }
 
-    # Return the custom object array
+    # Return the result array as output
     return  $TextRange
 }
