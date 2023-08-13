1fc22667eed8e1b6eb21dd3df6f926ae06d1d38a
perXautomatik <christoffer.broback@gmail.com>
2023-08-13 14:02:04 +0200

    ToGpt: can you modify the script, so that before it atempts to remove the submodule, it checks if the submodule is properly pushed to it's remote, if that is not the case, or if the check ressults in an error, stop the process unless -force flag was provided when calling this script, also if a --atempt-to-sync  flagg is provided, the script should trye to do a sync towards tracked remote with rebase or merge behaivor accordingly, if this process did not ressult in errors, and finished with a clean sync of remote, check again if sync is in state, then if completely synced, continue the process of removal

--- submoduleToWorktree.ps1
+++ submoduleToWorktree.ps1
@@ -3,7 +3,9 @@ function Remove-Submodule {
   param(
     [string]$SubmodulePath, # The path of the submodule
     [string]$SubmoduleName, # The name of the submodule (optional)
-    [string]$SubmoduleUrl   # The remote URL of the submodule (optional)
+    [string]$SubmoduleUrl,  # The remote URL of the submodule (optional)
+    [switch]$Force,         # A flag to force the removal even if the submodule is not properly pushed
+    [switch]$AttemptToSync  # A flag to attempt to sync the submodule with its remote before removing it
   )
 
   # Try to remove the submodule and catch any errors
@@ -74,6 +76,45 @@ function Remove-Submodule {
     # Register where the submodule stores its config file before removing it
     Push-Location $SubmodulePath # Change directory to the submodule path
     $ConfigFile = git rev-parse --git-path config # Get the config file path using git rev-parse[^1^][1]
+    
+    # Check if the submodule is properly pushed to its remote
+    $LocalCommit = git rev-parse HEAD # Get the local commit hash
+    $RemoteCommit = git rev-parse @{u} # Get the remote commit hash
+    if ($LocalCommit -ne $RemoteCommit) {
+      # If the commits are different, the submodule is not properly pushed
+      if ($Force) {
+        # If the force flag is given, ignore the difference and warn the user
+        Write-Warning "The submodule $SubmoduleName is not properly pushed to its remote. Proceeding with removal anyway."
+      }
+      elseif ($AttemptToSync) {
+        # If the attempt to sync flag is given, try to sync the submodule with its remote
+        Write-Host "The submodule $SubmoduleName is not properly pushed to its remote. Attempting to sync it."
+        $CurrentBranch = git rev-parse --abbrev-ref HEAD # Get the current branch name
+        $RemoteBranch = git rev-parse --abbrev-ref --symbolic-full-name @{u} # Get the remote branch name
+        if ($CurrentBranch -eq $RemoteBranch) {
+          # If the current branch and the remote branch are the same, use git pull to sync
+          git pull --rebase # Use rebase option to avoid merge commits
+        }
+        else {
+          # If the current branch and the remote branch are different, use git merge to sync
+          git merge $RemoteBranch # Merge the remote branch into the current branch
+        }
+        # Check again if the submodule is properly pushed to its remote
+        $LocalCommit = git rev-parse HEAD # Get the new local commit hash
+        $RemoteCommit = git rev-parse @{u} # Get the new remote commit hash
+        if ($LocalCommit -ne $RemoteCommit) {
+          # If the commits are still different, the sync failed
+          Write-Error "Failed to sync the submodule $SubmoduleName with its remote. Aborting removal."
+          exit 1
+        }
+      }
+      else {
+        # If neither flag is given, abort the removal and inform the user
+        Write-Error "The submodule $SubmoduleName is not properly pushed to its remote. Aborting removal. Use -Force or -AttemptToSync flags to override this behavior."
+        exit 1
+      }
+    }
+
     Pop-Location # Go back to the main repository directory
 
     # Deinit the submodule
