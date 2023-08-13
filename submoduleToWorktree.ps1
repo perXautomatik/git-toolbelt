9c67feec2c29f92acc0d708e2271c65238c5e3b4
perXautomatik <christoffer.broback@gmail.com>
2023-08-13 13:45:21 +0200

    ToGpt: can you modify Remove-Submodule function, so that in case $SubmoduleUrl is not provided, the script tryes to asign the currently checked out branch if it has a tracked remote branch then assigne that remote as $submoduleUrl, if that is not possible, look if any of registered remotes has a branch with same name as currently checked out in submodule, then assign (first occurence as $submoduleUrl, if that is not possible and the submodule has any remote, then select origin if present, otherwise take any remote and assign as $SubmoduleUrl, if no url assigned yet then, the script looks for a url entry for the submodule in the gitmdoule file of parent repo, alternativly in the parent repos .git/config, lastly if all else fails, $SubmoduleUrl = $SubmodulePath

--- submoduleToWorktree.ps1
+++ submoduleToWorktree.ps1
@@ -2,8 +2,8 @@
 function Remove-Submodule {
   param(
     [string]$SubmodulePath, # The path of the submodule
-    [string]$SubmoduleUrl,  # The remote URL of the submodule
-    [string]$SubmoduleName  # The name of the submodule (optional)
+    [string]$SubmoduleName, # The name of the submodule (optional)
+    [string]$SubmoduleUrl   # The remote URL of the submodule (optional)
   )
 
   # Try to remove the submodule and catch any errors
@@ -21,6 +21,53 @@ function Remove-Submodule {
       # If the name is still not found, try to get it from the last part of the submodule path
       if (!$SubmoduleName) {
         $SubmoduleName = Split-Path $SubmodulePath -Leaf
+      }
+    }
+
+    # If the submodule URL is not given, try to get it from different sources
+    if (!$SubmoduleUrl) {
+      # Change directory to the submodule path
+      Push-Location $SubmodulePath
+
+      # Try to get the URL of the currently checked out branch if it has a tracked remote branch
+      $SubmoduleUrl = git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>$null | ForEach-Object {git remote get-url $_.Split("/")[0]}
+
+      # If the URL is still not found, try to get the URL of any registered remote that has a branch with the same name as currently checked out in submodule
+      if (!$SubmoduleUrl) {
+        $CurrentBranch = git rev-parse --abbrev-ref HEAD
+        $RemoteName = git branch -r --contains $CurrentBranch | Select-Object -First 1 | ForEach-Object {$_ -split "/"} | Select-Object -First 1
+        if ($RemoteName) {
+          $SubmoduleUrl = git remote get-url $RemoteName
+        }
+      }
+
+      # If the URL is still not found, try to get the URL of any remote, preferring origin if present
+      if (!$SubmoduleUrl) {
+        $RemoteName = git remote | Where-Object {$_ -eq "origin"} | Select-Object -First 1
+        if (!$RemoteName) {
+          $RemoteName = git remote | Select-Object -First 1
+        }
+        if ($RemoteName) {
+          $SubmoduleUrl = git remote get-url $RemoteName
+        }
+      }
+
+      # If the URL is still not found, try to get it from the .gitmodules file using git config
+      if (!$SubmoduleUrl) {
+        Pop-Location # Go back to the main repository directory
+        $SubmoduleUrl = git config --file .gitmodules --get-regexp url | Where-Object {$_ -match $SubmoduleName} | ForEach-Object {$_ -split " "} | Select-Object -Last 1
+      }
+
+      # If the URL is still not found, try to get it from the .git/config file using git config
+      if (!$SubmoduleUrl) {
+        Pop-Location # Go back to the main repository directory
+        $SubmoduleUrl = git config --get-regexp url | Where-Object {$_ -match $SubmoduleName} | ForEach-Object {$_ -split " "} | Select-Object -Last 1
+      }
+
+      # If the URL is still not found, use the submodule path as a fallback
+      if (!$SubmoduleUrl) {
+        Pop-Location # Go back to the main repository directory
+        $SubmoduleUrl = $SubmodulePath
       }
     }
 
