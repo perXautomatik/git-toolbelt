c7f550fddd705df04148b63d5e1ebb66d2051ccd
Christoffer Brobäck <christoffer.broback@gmail.com>
2023-08-21 18:43:18 +0200

    can you show me an example of how i can use the chunk-file , hash-chunk and graph-chunk functions to create and view a graph from provided paths

--- workflow/Gephi/file-relationship.ps1
+++ workflow/Gephi/file-relationship.ps1
@@ -1,16 +1,35 @@
-# Define the parameters for the script
+# 1. Define the parameters for the script, such as the list of file paths to process, the number of lines in each chunk, and the optional output path to save the graph relationships as CSV files. For example:
 param(
     [Parameter(Mandatory=$true)]
-    [string[]]$FilePaths, # A list of file paths to process
+    [string[]]$FilePaths = @("C:\Users\Alice\Documents\file1.txt", "C:\Users\Bob\Documents\file2.txt"), # A list of file paths to process
     [Parameter(Mandatory=$true)]
-    [int]$ChunkSize, # The number of lines in each chunk
+    [int]$ChunkSize = 10, # The number of lines in each chunk
     [Parameter(Mandatory=$false)]
-    [string]$OutputPath # The path to save the graph relationships as CSV files
+    [string]$OutputPath = "C:\Users\Alice\Desktop\Graph" # The path to save the graph relationships as CSV files
 )
 
-# Import the required modules
-Import-Module Microsoft.PowerShell.Utility
-Import-Module Microsoft.PowerShell.GraphicalTools
+#2. Import the required modules for the script, such as Microsoft.PowerShell.Utility and Microsoft.PowerShell.GraphicalTools. For example:
+
+    # Import the required modules
+    Import-Module Microsoft.PowerShell.Utility
+    Import-Module Microsoft.PowerShell.GraphicalTools
+
+#3. Define the functions for the script, such as Chunk-File, Hash-Chunk and Graph-Chunk. You can use the code that I wrote for you in my previous answer.
+
+#4. Initialize an empty table to store the metadata of the files and chunks. For example:
+
+ 
+    # Initialize an empty table to store the metadata of the files and chunks
+    $MetadataTable = @{}
+ 
+
+#5. Initialize an empty table to store the graph nodes and edges. For example:
+
+
+    # Initialize an empty table to store the graph nodes and edges
+    $GraphTable = @{}
+
+
 
 # Define a function to chunk the content of a file into a set of lines
 function Chunk-File {
@@ -187,4 +206,109 @@ function Graph-Chunk {
             Create-Edge -SourceNode $NodeObject -TargetNode $OtherNode -Weight 0.5
         }
     }
+}
+
+
+<#6. Loop through each file path in the input list, and perform the following actions for each file:
+
+    - Check if the file path is valid and exists. If it is not, write an error message and continue to the next file path.
+    - Chunk the file content into a set of lines using the Chunk-File function with the chunk size parameter.
+    - Loop through each chunk in the array of chunks, and perform the following actions for each chunk:
+        - Hash the chunk and get its hash string and file id using the Hash-Chunk function with the metadata table parameter.
+        - Graph the chunk and create relationships between nodes using the Graph-Chunk function with the graph table parameter.
+
+7. Check if an output path parameter is provided. If it is, save the graph relationships as CSV files for Gephi to import using ConvertTo-Csv and Out-File cmdlets. If it is not, display the graph using Out-GridView cmdlet with appropriate options.
+
+Here is an example of how you can write this part of the script:
+#>
+
+# Loop through each file path in the input list
+foreach ($FilePath in $FilePaths) {
+    # Check if the file path is valid and exists
+    if (Test-Path -Path $FilePath) {
+        # If it is, chunk the file content into a set of lines using the chunk size parameter
+        $Chunks = Chunk-File -FilePath $FilePath -ChunkSize $ChunkSize
+
+        # Loop through each chunk in the array of chunks
+        foreach ($Chunk in $Chunks) {
+            # Hash the chunk and get its hash string and file id using the metadata table parameter
+            ($ChunkHash, $FileId) = Hash-Chunk -Chunk $Chunk -FilePath $FilePath -MetadataTable $MetadataTable
+
+            # Graph the chunk and create relationships between nodes using the graph table parameter 
+            Graph-Chunk -ChunkHash $ChunkHash -FileId $FileId -GraphTable $GraphTable 
+        }
+    }
+    else {
+        # If it is not, write an error message and continue to the next file path
+        Write-Error "The file path '$FilePath' is invalid or does not exist."
+    }
+}
+
+# Check if an output path parameter is provided 
+if ($OutputPath) {
+    # If it is, save the graph relationships as CSV files for Gephi to import
+
+    # Create an empty array to store the nodes as custom objects with id and label properties 
+    $NodesArray = @()
+
+    # Loop through each node in the graph table 
+    foreach ($Node in $GraphTable.Values) {
+        # Create a new node object with its id and label properties 
+        $NodeObject = New-Object -TypeName PSObject -Property @{
+            Id = $Node.Id 
+            Label = "Chunk: $($Node.Id)"
+        }
+
+        # Add the node object to the nodes array
+        $NodesArray += $NodeObject
+    }
+
+    # Create an empty array to store the edges as custom objects with source, target, and weight properties
+    $EdgesArray = @()
+
+    # Loop through each node in the graph table 
+    foreach ($Node in $GraphTable.Values) {
+        # Loop through each edge in the node's edges array
+        foreach ($Edge in $Node.Edges) {
+            # Add the edge object to the edges array
+            $EdgesArray += $Edge
+        }
+    }
+
+    # Convert the nodes array and the edges array to CSV format
+    $NodesCSV = $NodesArray | ConvertTo-Csv -NoTypeInformation
+    $EdgesCSV = $EdgesArray | ConvertTo-Csv -NoTypeInformation
+
+    # Save the CSV files to the output path with appropriate names
+    $NodesCSV | Out-File -FilePath "$OutputPath\nodes.csv" -Encoding UTF8
+    $EdgesCSV | Out-File -FilePath "$OutputPath\edges.csv" -Encoding UTF8
+
+    # Write a success message and the output path
+    Write-Host "The graph relationships have been saved as CSV files to '$OutputPath'. You can import them to Gephi for visualization."
+}
+else {
+    # If it is not, display the graph using Out-GridView cmdlet
+
+    # Create an empty array to store the graph objects with id, label, and group properties
+    $GraphArray = @()
+
+    # Loop through each node in the graph table 
+    foreach ($Node in $GraphTable.Values) {
+        # Create a new graph object with its id, label, and group properties 
+        # The group property is used to color the nodes by their file ids
+        $GraphObject = New-Object -TypeName PSObject -Property @{
+            Id = $Node.Id 
+            Label = "Chunk: $($Node.Id)"
+            Group = ($Node.FileIds | Sort-Object | Get-Unique) -join ", "
+        }
+
+        # Add the graph object to the graph array
+        $GraphArray += $GraphObject
+    }
+
+    # Display the graph array using Out-GridView cmdlet with appropriate options
+    $GraphArray | Out-GridView -Title "Relationship Graph" -OutputMode None
+
+    # Write a success message and a tip for exploring the graph
+    Write-Host "The relationship graph has been displayed using Out-GridView. You can sort, filter, and group the nodes by their properties."
 }
